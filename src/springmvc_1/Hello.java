3df9ca4a4694290d23023c08ee33a8b91c62c02a
tongwei <wzdsx2012@qq.com>
2019-05-12 23:09:32 +0800

    test2

--- src/springmvc_1/Hello.java
+++ src/springmvc_1/Hello.java
@@ -2,6 +2,6 @@ package springmvc_1;
 
 public class Hello {
 	public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World随便改一改");
     }
 }
