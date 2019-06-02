716dc4490a6dbe144e148d795c83ac94277a270c
Administrator <Administrator@USER-20190513HT>
2019-06-02 09:59:07 +0800

    dsfgd

--- src/springmvc_1/Hello.java
+++ src/springmvc_1/Hello.java
@@ -2,6 +2,6 @@ package springmvc_1;
 
 public class Hello {
 	public static void main(String[] args) {
-        System.out.println("Hello World随便改一改  yyyyyy   的规范地方  我除非VB发  是递归丰东股份");
+        System.out.println("Hello World随便改一改  yyyyyy ngbhng  的规范地方  我除非VB发  是递归丰东股份");
     }
 }
